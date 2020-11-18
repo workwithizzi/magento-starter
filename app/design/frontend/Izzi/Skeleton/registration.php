16f24fdc89c1d0ae4888b9f1d87ed8bdcc67048f
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-11-18 16:20:48 -0600

    updated the skeleton directory name

--- app/design/frontend/Izzi/Skeleton/registration.php
+++ app/design/frontend/Izzi/Skeleton/registration.php
@@ -5,6 +5,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 // Register the theme, which is considered a component, with Magento
 ComponentRegistrar::register(
     ComponentRegistrar::THEME, // The type of component to register
-      'frontend/Izzi/Skeleton', // The path to your theme relative to app/design
+      'frontend/Izzi/skeleton', // The path to your theme relative to app/design
     __DIR__ // Look at the current working directory for the component
 );
